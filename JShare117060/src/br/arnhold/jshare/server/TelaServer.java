be090f2fd487933edc5d37576ea7280cf53b6989
rafael <rafael@arnhold>
2016-05-04 21:03:59 -0300

    Parando serviço servidor

--- JShare117060/src/br/arnhold/jshare/server/TelaServer.java
+++ JShare117060/src/br/arnhold/jshare/server/TelaServer.java
@@ -1,6 +1,5 @@
 package br.arnhold.jshare.server;
 
-
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -18,6 +17,7 @@ import javax.swing.JTextField;
 import java.awt.Insets;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
+import java.rmi.NoSuchObjectException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -31,7 +31,7 @@ import javax.swing.JScrollPane;
 import java.awt.Color;
 import java.awt.Font;
 
-public class TelaServer extends JFrame  implements IServer {
+public class TelaServer extends JFrame implements IServer {
 
 	private JPanel contentPane;
 	private JTextField txtIp;
@@ -40,9 +40,10 @@ public class TelaServer extends JFrame  implements IServer {
 	private JButton btnIniciaServico;
 	private JButton btnFecharServico;
 	private IServer servidor;
-	
-	//Lista de cliente
-	private List<Cliente> listaCliente = new ArrayList<>(); 
+	private Registry registry;
+
+	// Lista de cliente
+	private List<Cliente> listaCliente = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -71,12 +72,13 @@ public class TelaServer extends JFrame  implements IServer {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
-		
+
 		JLabel lblIp = new JLabel("IP:");
 		GridBagConstraints gbc_lblIp = new GridBagConstraints();
 		gbc_lblIp.anchor = GridBagConstraints.WEST;
@@ -84,7 +86,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_lblIp.gridx = 0;
 		gbc_lblIp.gridy = 0;
 		contentPane.add(lblIp, gbc_lblIp);
-		
+
 		txtIp = new JTextField();
 		txtIp.setText("127.0.0.1");
 		GridBagConstraints gbc_txtIp = new GridBagConstraints();
@@ -95,7 +97,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_txtIp.gridy = 0;
 		contentPane.add(txtIp, gbc_txtIp);
 		txtIp.setColumns(10);
-		
+
 		JLabel lblPorta = new JLabel("Porta:");
 		GridBagConstraints gbc_lblPorta = new GridBagConstraints();
 		gbc_lblPorta.anchor = GridBagConstraints.WEST;
@@ -103,7 +105,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_lblPorta.gridx = 0;
 		gbc_lblPorta.gridy = 1;
 		contentPane.add(lblPorta, gbc_lblPorta);
-		
+
 		txtPorta = new JTextField();
 		txtPorta.setText("1001");
 		GridBagConstraints gbc_txtPorta = new GridBagConstraints();
@@ -114,7 +116,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_txtPorta.gridy = 1;
 		contentPane.add(txtPorta, gbc_txtPorta);
 		txtPorta.setColumns(10);
-		
+
 		btnIniciaServico = new JButton("Inicia Serviço");
 		btnIniciaServico.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -126,7 +128,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_btnIniciaServico.gridx = 4;
 		gbc_btnIniciaServico.gridy = 1;
 		contentPane.add(btnIniciaServico, gbc_btnIniciaServico);
-		
+
 		btnFecharServico = new JButton("Parar Serviço");
 		btnFecharServico.setEnabled(false);
 		btnFecharServico.addActionListener(new ActionListener() {
@@ -139,7 +141,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_btnFecharServico.gridx = 5;
 		gbc_btnFecharServico.gridy = 1;
 		contentPane.add(btnFecharServico, gbc_btnFecharServico);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.gridwidth = 10;
@@ -147,7 +149,7 @@ public class TelaServer extends JFrame  implements IServer {
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 2;
 		contentPane.add(scrollPane, gbc_scrollPane);
-		
+
 		txtApresentacao = new JTextArea();
 		txtApresentacao.setFont(new Font("Dialog", Font.BOLD, 12));
 		txtApresentacao.setBackground(Color.BLACK);
@@ -156,55 +158,56 @@ public class TelaServer extends JFrame  implements IServer {
 		scrollPane.setViewportView(txtApresentacao);
 	}
 
-	protected void pararServico() {
-		btnIniciaServico.setEnabled(true);
-		// TODO Auto-generated method stubtnFecharServico.setEnabled(false);
-		txtIp.setEnabled(true);
-		txtPorta.setEnabled(true);
-		
-		
-		
-		txtApresentacao.append("Serviço Parado \n");
-		
-		
-	}
-
 	private void iniciarServidor() {
 		btnIniciaServico.setEnabled(false);
 		btnFecharServico.setEnabled(true);
 		txtIp.setEnabled(false);
 		txtPorta.setEditable(false);
-		
+
 		try {
-			servidor = (IServer) UnicastRemoteObject.exportObject(this,0);
-			Registry registry = LocateRegistry.createRegistry(Integer.parseInt(txtPorta.getText().trim()));
+			servidor = (IServer) UnicastRemoteObject.exportObject(this, 0);
+			registry = LocateRegistry.createRegistry(Integer.parseInt(txtPorta.getText().trim()));
 			registry.rebind(IServer.NOME_SERVICO, servidor);
-		
+
 		} catch (RemoteException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		txtApresentacao.append("Serviço iniciado \n");	
-		
-		
+
+		txtApresentacao.append("Serviço iniciado \n");
+
 	}
 
-	
+	protected void pararServico() {
+		btnIniciaServico.setEnabled(true);
+		// TODO Auto-generated method stubtnFecharServico.setEnabled(false);
+		txtIp.setEnabled(true);
+		txtPorta.setEnabled(true);
+
+		try {
+			UnicastRemoteObject.unexportObject(this, true);
+			UnicastRemoteObject.unexportObject(registry, true);
+		} catch (NoSuchObjectException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		txtApresentacao.append("Serviço Parado \n");
+
+	}
+
 	@Override
 	public void registrarCliente(Cliente c) throws RemoteException {
 		listaCliente.add(c);
-		
-		txtApresentacao.append("Cliente "+ c.getNome() +" adicionado com sucesso");
-		
+
+		txtApresentacao.append("Cliente " + c.getNome() + " adicionado com sucesso");
+
 	}
 
 	@Override
 	public void publicarListaArquivos(Cliente c, List<Arquivo> lista) throws RemoteException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -222,9 +225,9 @@ public class TelaServer extends JFrame  implements IServer {
 	@Override
 	public void desconectar(Cliente c) throws RemoteException {
 		listaCliente.remove(c);
-		
-		txtApresentacao.append("Cliente "+ c.getNome() +" removido com sucesso");
-		
+
+		txtApresentacao.append("Cliente " + c.getNome() + " removido com sucesso");
+
 	}
 
 }
